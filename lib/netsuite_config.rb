cfa65c4fa9ed30fea9f3a12469037e6981e7f8fe
Michael Bianco <iloveitaly@gmail.com>
2015-01-27 14:09:58 -0500

    Adding type mapping for all file types

--- lib/netsuite_config.rb
+++ lib/netsuite_config.rb
@@ -5,13 +5,20 @@ require 'base64'
 module NetSuiteConfig
   extend self
 
+  TYPE_MAPPING = {
+    '_JAVASCRIPT' => 'suite-script',
+    '_PDF' => 'pdf',
+    '_HTMLDOC' => 'html',
+    '_MISCTEXT' => 'text',
+    '_JPGIMAGE' => 'images/jpeg',
+    '_PJPGIMAGE' => 'images/jpeg',
+    '_GIFIMAGE' => 'images/gif',
+    '_PNGIMAGE' => 'images/png',
+    '_MISCBINARY' => 'binary',
+    '_EXCEL' => 'excel' 
+  }
+
   def pull
-    dump_directory = File.join(Dir.pwd, 'suite-scripts')
-
-    unless File.exists?(dump_directory)
-      Dir.mkdir dump_directory
-    end
-
     search = NetSuite::Records::File.search({
       preferences: {
         bodyFieldsOnly: false,
@@ -21,16 +28,21 @@ module NetSuiteConfig
 
     search.results_in_batches do |batch|
       batch.each do |file|
-        next unless file.file_type == "_JAVASCRIPT"
+        unless TYPE_MAPPING.has_key?(file.file_type)
+          raise "type not handled #{file.file_type}"
+        end
 
-        # _HTMLDOC
-        # _PDF
+        output_directory = File.join(Dir.pwd, TYPE_MAPPING[file.file_type])
+
+        unless File.exists?(output_directory)
+          FileUtils.mkdir_p output_directory
+        end
 
         begin
           full_file = NetSuite::Records::File.get file.internal_id
 
           if full_file
-            File.open(File.join(dump_directory, full_file.name), 'w') { |f| f.write(Base64.decode64(full_file.content)) }
+            File.open(File.join(output_directory, full_file.name), 'w') { |f| f.write(Base64.decode64(full_file.content)) }
           else
             puts "Error downloading file: #{file.internal_id}"
           end
