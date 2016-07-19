5fe15bab0789895832ab6bdea0b122e04d093b2c
Michael Bianco <iloveitaly@gmail.com>
2016-07-19 14:06:43 -0400

    Allow wsdl_domain to be specified via env vars

--- lib/netsuite_config/netsuite_init.rb
+++ lib/netsuite_config/netsuite_init.rb
@@ -1,13 +1,18 @@
 NetSuite.configure do
   reset!
-  
+
+  read_timeout  100_000
+
   email         ENV['NETSUITE_EMAIL']
   password      ENV['NETSUITE_PASSWORD']
   account       ENV['NETSUITE_ACCOUNT']
   role          ENV['NETSUITE_ROLE']
 
   api_version   ENV['NETSUITE_API']
-  read_timeout  100000
   sandbox       ENV['NETSUITE_PRODUCTION'].nil? || ENV['NETSUITE_PRODUCTION'] != 'true'
   # log           File.join(Rails.root, 'log/netsuite.log')
+
+  if ENV['NETSUITE_WSDL_DOMAIN']
+    wsdl_domain ENV['NETSUITE_WSDL_DOMAIN']
+  end
 end
